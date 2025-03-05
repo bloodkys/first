515ca0bba77390ecad5f375ebdc27368475bb6e1
bloodkys <bloodkys@gmail.com>
2025-03-05 14:57:55 +0900

    내 첫작품

--- ilsan/it/views.py
+++ ilsan/it/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {"name1": "김영석", "name2": "홍길동", "name3": "아무개"}
+    context = {
+        "name1": "김영석",
+        "name2": "홍길동",
+        "name3": "아무개",
+        "food": "연어초밥",
+        "hobby": "음악감상",
+        "interest": "게임",
+    }
     # render(html파일을 연결시켜주는 라이브러리)
     return render(request, "it/home.html", context)
